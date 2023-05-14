0a35064583a57b046aac6d51d485978acec5d3eb
xp3rt <127796811+reeexpert@users.noreply.github.com>
2023-05-14 08:17:12 +0300

    Update useText.tsx

--- src/hooks/useText.tsx
+++ src/hooks/useText.tsx
@@ -53,7 +53,7 @@ function handleUrl(key: string, result: string[]) {
   return (
     <span key={key}>
       {' '}
-      <span className="fake-link mention"> <a target="_blank" rel="noopener noreferrer" href={result[1]}> {result[1]}</a></span>
+      <span className="fake-link mention"> <a target="_blank" rel="noopener noreferrer" href={'https://twitter.com/' + result[1]}> {result[1]}</a></span>
     </span>
   );
 }
